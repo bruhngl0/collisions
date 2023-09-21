6cf23d8bf07e57f7b7eb395c259d913e2a6b3344
bruh.ngl0 <adityashrm500@gmail.com>
2023-09-21 16:16:20 +0530

    message

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+/*import * as THREE from 'three';
 import * as CANNON from 'cannon';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 
@@ -51,7 +51,7 @@ controls.addEventListener('unlock', function () {
 /*const wallGeometry = new THREE.BoxGeometry(10, 10, 0.5);
 const wallMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const wall = new THREE.Mesh(wallGeometry, wallMaterial);
-scene.add(wall); */
+scene.add(wall); 
 
 // Add a floor
 const floorGeometry = new THREE.BoxGeometry(50, 0.1, 50);
@@ -69,7 +69,7 @@ world.gravity.set(0, -9.82, 0); // Gravity along the y-axis
 /*const wallShape = new CANNON.Box(new CANNON.Vec3(5, 2.5, 0.25));
 const wallBody = new CANNON.Body({ mass: 0 }); // Making it static
 wallBody.addShape(wallShape);
-world.addBody(wallBody); */
+world.addBody(wallBody); 
 
 // Create floor body
 const floorShape = new CANNON.Box(new CANNON.Vec3(25, 0.05, 25));
@@ -162,4 +162,152 @@ const animate = () => {
     renderer.render(scene, camera);
   }
   
-  animate()
+  animate() */
+
+ 
+
+// Set up the scene, camera, and renderer
+
+//code for layer assignment
+
+/*import * as THREE from 'three'
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.y = 2; // Adjust the height as required.
+camera.position.z = 5; 
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+// Create two cubes with different colors
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const redMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const blueMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+
+const redCube = new THREE.Mesh(geometry, redMaterial);
+const blueCube = new THREE.Mesh(geometry, blueMaterial);
+
+scene.add(redCube);
+scene.add(blueCube);
+
+redCube.visible = false;
+blueCube.visible = false;
+
+camera.position.z = 5;
+
+// Render loop
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
+
+// Add event listeners to buttons
+document.getElementById('showRed').addEventListener('click', () => {
+    redCube.visible = true;
+    blueCube.visible = false;
+});
+
+document.getElementById('showBlue').addEventListener('click', () => {
+    redCube.visible = false;
+    blueCube.visible = true;
+}); */
+
+
+
+//GARUDA GAMANA RISHABA VAHANA 
+
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+const cubeTextureLoader = new THREE.CubeTextureLoader
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.y = 2; // Adjust the height as required.
+camera.position.z = 5; 
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const textureLoader = new THREE.TextureLoader();
+
+const environmentMap = cubeTextureLoader.load([
+    '/environmentMaps/2/px.png',
+    '/environmentMaps/2/nx.png',
+    '/environmentMaps/2/py.png',
+    '/environmentMaps/2/ny.png',
+    '/environmentMaps/2/pz.png',
+    '/environmentMaps/2/nz.png',
+])
+
+
+scene.environment = environmentMap
+scene.background = environmentMap
+
+let and;
+const loader = new GLTFLoader();
+
+
+loader.load('test.glb', (gltf) => {
+    gltf.scene.traverse((child) => {
+        if (child.isMesh) {
+            and = child; // This will get your mesh, even if nested
+        }
+    });
+
+    if (!and) {
+        console.error("No mesh found in the GLTF model!");
+        return;
+    }
+
+    scene.add(gltf.scene);
+
+})
+// Function to apply a texture to the wall
+function applyTextureToWall(texturePath) {
+    textureLoader.load(texturePath, (texture) => {
+        and.material.map = texture;
+        and.material.needsUpdate = true; // Important to update the material
+    });
+}
+
+// Event listeners for buttons
+document.getElementById('texture1Btn').addEventListener('click', () => {
+    applyTextureToWall('/environmentMaps/2/px.png');
+});
+
+document.getElementById('texture2Btn').addEventListener('click', () => {
+    applyTextureToWall('/environmentMaps/2/nx.png');
+});
+
+camera.position.z = 5;
+
+
+const controls = new PointerLockControls(camera, document.body);
+
+document.addEventListener('click', function () {
+    controls.lock();
+});
+
+controls.addEventListener('lock', function () {
+    // Handle lock - for example, hide some UI element
+});
+
+controls.addEventListener('unlock', function () {
+    // Handle unlock - for example, show some UI element
+});
+
+
+// Render loop
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
+
+
